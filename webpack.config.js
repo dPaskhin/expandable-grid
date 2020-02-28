92b9ca056c9c0b46609e0774c738460d56ee0203
Дмитрий Пасхин <d.pasxin@maximaster.ru>
2020-02-29 01:44:43 +0300

    Создал структуру проекта, установил необходимые пакеты, добавил конфиги для webpack'a и ts'a. Добавил README.md и LICENSE.md

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require('path')
+
+const HTMLPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const outPutPath = './dist'
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+const isDev = NODE_ENV === 'development';
+const isProd = NODE_ENV === 'production'
+
+
+module.exports = {
+    entry: {
+        bundle: './example/index.tsx'
+    },
+    output: {
+        path: path.resolve(__dirname, outPutPath)
+    },
+    devtool: isDev && 'source-map',
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+    optimization: {
+        minimize: isProd
+    },
+    plugins: [
+        new HTMLPlugin({
+            template: './example/index.html',
+            filename: 'index.html',
+            chunks: ['bundle']
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin()
+    ],
+    devServer: {
+        hot: true,
+        contentBase: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                loader: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                '@babel/preset-env',
+                                '@babel/preset-typescript'
+                            ]
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.tsx$/,
+                exclude: /node_modules/,
+                loader: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react',
+                            '@babel/preset-typescript'
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                ],
+            }
+        ]
+    }
+}
