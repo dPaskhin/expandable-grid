35ca7f6f521d26342e802004f712ad04bc84d41a
Dmitry Dova <mi.dovaah@gmail.com>
2020-09-05 00:19:38 +0300

    v1.1.3 Fix bug with wrong import of enums

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 require('./lib/expandableGrid.css');
 var expandableGrid = require('./lib/expandableGrid');
 
+expandableGrid.DimensionsTypes = {
+    ITEM_HEIGHT: 'itemHeight',
+    COLUMNS_COUNT: 'columnsCount',
+    ROW_GAP: 'rowGap',
+    COLUMN_GAP: 'columnGap',
+    EXPANDED_ITEM_HEIGHT: 'expandedItemHeight',
+}
+
+expandableGrid.EntityTypes = {
+    GRID: 'grid',
+    ITEM: 'item',
+}
+
 module.exports = expandableGrid;
